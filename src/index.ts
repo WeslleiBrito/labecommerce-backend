7d66d822bc704ab572a1fe97af4573493ad76040
WeslleiBrito <wesllei.wbs@gmail.com>
2023-06-06 11:56:02 -0300

    Exercicio 3

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express'
 import { users, products } from './database'
 
 import cors from 'cors'
+import { TCreateUser, TProducts, TUsers } from './types'
 
 
 const app = express()
@@ -20,7 +21,7 @@ app.get('/users', (req: Request, res: Response) => {
     res.status(200).send(users)
 })
 
-//Buscar todos os produtos
+//Buscar todos os produtos ou um produto expecifico
 app.get('/products/search', (req: Request, res: Response) => {
     const filter = req.query.filter as string
 
@@ -33,8 +34,42 @@ app.get('/products/search', (req: Request, res: Response) => {
     } else {
         res.status(200).send(products)
     }
+})
 
+//Criar um novo usuário 
+app.post('/users', (req: Request, res: Response) => {
 
+    const { id, name, email, password }: { id: string, name: string, email: string, password: string } = req.body
+
+    const newUser: TUsers = {
+        id,
+        name,
+        email,
+        password,
+        createdAt: `${new Date().toISOString()}`
+    }
+
+    users.push(newUser)
+
+    res.status(200).send(`${name}, seu cadastro foi realizado com sucesso!`)
+
+})
+
+//Criar um novo produto
+app.post('/products', (req: Request, res: Response) => {
+    const { id, name, price, description, imageUrl }: TProducts = req.body
+
+    products.push(
+        {
+            id,
+            name,
+            price,
+            description,
+            imageUrl
+        }
+    )
+
+    res.status(201).send(`O produto ${name}, foi cadastrado com sucesso!`)
 })
 
 app.listen(3003, () => {
