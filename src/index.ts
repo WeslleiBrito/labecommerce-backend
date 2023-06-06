60f0107eaff152d5d3e46209bb11227e41a9a76c
WeslleiBrito <wesllei.wbs@gmail.com>
2023-06-06 10:44:57 -0300

    Exercicio 2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from 'express'
+import { users, products } from './database'
+
 import cors from 'cors'
 
 
@@ -11,6 +13,28 @@ app.use(cors())
 
 app.get('/ping', (req: Request, res: Response) => {
     res.status(200).send("Pong!")
+})
+
+//Buscar todos os usuários
+app.get('/users', (req: Request, res: Response) => {
+    res.status(200).send(users)
+})
+
+//Buscar todos os produtos
+app.get('/products/search', (req: Request, res: Response) => {
+    const filter = req.query.filter as string
+
+    if (filter) {
+        res.send(
+            products.filter((product) => {
+                return product.name.toLowerCase().includes(filter.toLowerCase())
+            })
+        )
+    } else {
+        res.status(200).send(products)
+    }
+
+
 })
 
 app.listen(3003, () => {
