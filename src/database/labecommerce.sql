f4f76df8f24fa02a9289a1d85fab984d8c30cb72
WeslleiBrito <wesllei.wbs@gmail.com>
2023-06-20 10:08:54 -0300

    Exercicio 2

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,1 +1,38 @@
 -- Active: 1687263910913@@127.0.0.1@3306
+
+CREATE TABLE
+    IF NOT EXISTS users (
+        id TEXT PRIMARY KEY NOT NULL,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL,
+        create_at TEXT NOT NULL
+    );
+
+INSERT INTO
+    users (
+        id,
+        name,
+        email,
+        password,
+        create_at
+    )
+VALUES (
+        "u001",
+        "Antonia",
+        "antonia@gmail.com",
+        "pas001",
+        "2020-03-15"
+    ), (
+        "u002",
+        "Wesllei",
+        "wesllei@gmail.com",
+        "pas002",
+        "2023-10-08"
+    ), (
+        "u003",
+        "Pâmela",
+        "pamela@gmail.com",
+        "pas003",
+        "2023-11-30"
+    );
