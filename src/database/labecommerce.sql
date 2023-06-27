26fcfeec5ec355613ab8dc9525dfd4400399d1ad
WeslleiBrito <wesllei.wbs@gmail.com>
2023-06-27 09:42:14 -0300

    Exercicio finalizado

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,8 +1,8 @@
--- Active: 1687447455369@@127.0.0.1@3306
+-- Active: 1687865300366@@127.0.0.1@3306
 
 CREATE TABLE
     IF NOT EXISTS users (
-        id TEXT PRIMARY KEY NOT NULL,
+        id TEXT PRIMARY KEY NOT NULL UNIQUE,
         name TEXT NOT NULL,
         email TEXT NOT NULL,
         password TEXT NOT NULL,
@@ -89,39 +89,108 @@ Venha conferir!",
     );
 
 -- Retorna todos os usuários cadastrados
+
 SELECT * FROM users;
 
 -- Retorna todos os produtros cadastrados
+
 SELECT * FROM products;
 
 -- Retorda todos os produtos que atendam a um nome especifico
+
 SELECT * FROM products WHERE name LIKE "%gamer%";
 
 -- Incluir um novo usuário
-INSERT INTO users (id, name, email, password, create_at) VALUES ("u004", "Beatriz", "bia.beatriz@hotmail.com", "pas004", "2021-11-15");
+
+INSERT INTO
+    users (
+        id,
+        name,
+        email,
+        password,
+        create_at
+    )
+VALUES (
+        "u004",
+        "Beatriz",
+        "bia.beatriz@hotmail.com",
+        "pas004",
+        "2021-11-15"
+    );
 
 -- Incluir um novo produto
-INSERT INTO products (id, name, description, price, image_url) VALUES 
-(
-    "prod006",
-    "Apple iPhone 14 Pro Max 128GB Roxo-profundo 6,7” 48MP",
-    "iPhone 14 Pro Max. Câmera grande-angular de 48 MP para capturar detalhes impressionantes. Dynamic Island, uma nova forma de interação no iPhone. Tela Sempre Ativa. E Detecção de Acidente*, um novo recurso de segurança que liga para a emergência se você não puder. Avisos legais *O SOS de Emergência usa uma conexão de rede celular ou chamadas Wi-Fi. **A tela tem bordas arredondadas. Quando medida como um retângulo, a tela tem 6,69 polegadas na diagonal. A área real de visualização é menor. ***A duração da bateria varia de acordo com o uso e a configuração. Consulte apple.com/br/batteries para obter mais informações. ****É preciso ter um plano de dados. 5G só está disponível em alguns países e por meio de determinadas operadoras. As velocidades variam de acordo com as condições e operadoras locais. Para obter detalhes sobre a compatibilidade com 5G, entre em contato com sua operadora e consulte apple.com/br/iphone/cellular. *****O iPhone 14 Pro Max é resistente a respingos, água e poeira e foi testado em condições controladas em laboratório, classificado como IP68 segundo a norma IEC 60529 (profundidade máxima de seis metros por até 30 minutos). A resistência a respingos, água e poeira não é uma condição permanente e pode diminuir com o tempo. Não tente recarregar um iPhone molhado. Veja instruções de limpeza e secagem no Manual do Usuário. Danos decorrentes de contato com líquidos não estão incluídos na garantia. ******Alguns recursos podem não estar disponíveis em todos os países ou regiões.",
-    7998.99,
-    "https://a-static.mlcdn.com.br/800x560/apple-iphone-14-pro-max-128gb-roxo-profundo-67-48mp/magazineluiza/235925100/954bf7dec1cbbd5eafee5ce7b3fdf7f8.jpg"
-);
+
+INSERT INTO
+    products (
+        id,
+        name,
+        description,
+        price,
+        image_url
+    )
+VALUES (
+        "prod006",
+        "Apple iPhone 14 Pro Max 128GB Roxo-profundo 6,7” 48MP",
+        "iPhone 14 Pro Max. Câmera grande-angular de 48 MP para capturar detalhes impressionantes. Dynamic Island, uma nova forma de interação no iPhone. Tela Sempre Ativa. E Detecção de Acidente*, um novo recurso de segurança que liga para a emergência se você não puder. Avisos legais *O SOS de Emergência usa uma conexão de rede celular ou chamadas Wi-Fi. **A tela tem bordas arredondadas. Quando medida como um retângulo, a tela tem 6,69 polegadas na diagonal. A área real de visualização é menor. ***A duração da bateria varia de acordo com o uso e a configuração. Consulte apple.com/br/batteries para obter mais informações. ****É preciso ter um plano de dados. 5G só está disponível em alguns países e por meio de determinadas operadoras. As velocidades variam de acordo com as condições e operadoras locais. Para obter detalhes sobre a compatibilidade com 5G, entre em contato com sua operadora e consulte apple.com/br/iphone/cellular. *****O iPhone 14 Pro Max é resistente a respingos, água e poeira e foi testado em condições controladas em laboratório, classificado como IP68 segundo a norma IEC 60529 (profundidade máxima de seis metros por até 30 minutos). A resistência a respingos, água e poeira não é uma condição permanente e pode diminuir com o tempo. Não tente recarregar um iPhone molhado. Veja instruções de limpeza e secagem no Manual do Usuário. Danos decorrentes de contato com líquidos não estão incluídos na garantia. ******Alguns recursos podem não estar disponíveis em todos os países ou regiões.",
+        7998.99,
+        "https://a-static.mlcdn.com.br/800x560/apple-iphone-14-pro-max-128gb-roxo-profundo-67-48mp/magazineluiza/235925100/954bf7dec1cbbd5eafee5ce7b3fdf7f8.jpg"
+    );
 
 -- Deletar um usuario por id
+
 DELETE FROM users WHERE id = 'u004';
 
 -- Deletar um produto com base em seu id
+
 DELETE FROM products WHERE id = "prod006";
 
 -- Editar um produto
-UPDATE products SET 
-id = "prod007", 
-name = "Apple Iphone 14 Pro Max 512GB Roxo-profundo 6,7”",
-description = "Uma nova forma de interação no seu iPhone. Um recurso essencial de segurança projetado para salvar vidas. Câmera grande-angular inovadora de 48 MP. Tela duas vezes mais brilhante sob a luz do sol◊Consultar avisos legais. Tudo com a potência do chip para smartphone que é o máximo.", 
-price = 10799.10,
-image_url =  "https://a-static.mlcdn.com.br/800x560/apple-iphone-14-pro-max-128gb-roxo-profundo-67-48mp/magazineluiza/235925100/954bf7dec1cbbd5eafee5ce7b3fdf7f8.jpg"
- WHERE id = "prod006";
 
+UPDATE products
+SET
+    id = "prod007",
+    name = "Apple Iphone 14 Pro Max 512GB Roxo-profundo 6,7”",
+    description = "Uma nova forma de interação no seu iPhone. Um recurso essencial de segurança projetado para salvar vidas. Câmera grande-angular inovadora de 48 MP. Tela duas vezes mais brilhante sob a luz do sol◊Consultar avisos legais. Tudo com a potência do chip para smartphone que é o máximo.",
+    price = 10799.10,
+    image_url = "https://a-static.mlcdn.com.br/800x560/apple-iphone-14-pro-max-128gb-roxo-profundo-67-48mp/magazineluiza/235925100/954bf7dec1cbbd5eafee5ce7b3fdf7f8.jpg"
+WHERE id = "prod006";
+
+CREATE TABLE
+    IF NOT EXISTS purchases (
+        id TEXT PRIMARY KEY NOT NULL UNIQUE,
+        buyer TEXT NOT NULL,
+        total_price REAL NOT NULL,
+        created_at TEXT NOT NULL,
+        FOREIGN KEY (buyer) REFERENCES users(id)
+    );
+
+INSERT INTO
+    purchases (
+        id,
+        buyer,
+        total_price,
+        created_at
+    )
+VALUES (
+        "pur001",
+        "u004",
+        560.00,
+        "2021-03-13"
+    ), (
+        "pur002",
+        "u001",
+        738.00,
+        "2019-05-28"
+    ), (
+        "pur003",
+        "u002",
+        53.90,
+        "2023-01-19"
+    ), (
+        "pur004",
+        "u003",
+        1639.19,
+        "2020-12-23"
+    );
+
+SELECT * FROM purchases;
