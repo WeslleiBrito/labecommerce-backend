e794f27975101489db226b09601c48700212d150
WeslleiBrito <wesllei.wbs@gmail.com>
2023-07-06 11:59:20 -0300

    Exercico 1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
-import { users, products } from './database'
 import { db } from './database/knex'
 import cors from 'cors'
+
 
 const app = express()
 
@@ -25,8 +25,7 @@ app.post('/users', async (req: Request, res: Response) => {
             id,
             name,
             email,
-            password,
-            createdAt: `${new Date().toISOString()}`
+            password
         }
 
         Object.entries(newUser).map((item) => {
@@ -38,23 +37,20 @@ app.post('/users', async (req: Request, res: Response) => {
             }
         })
 
+        const [idExist] = await db('users').where({ id })
+        const [emailExist] = await db('users').where({ email })
 
-        const idExist = await db.raw(`SELECT id FROM users WHERE id = "${id}";`)
-        const emailExist = await db.raw(`SELECT email FROM users WHERE email = "${email}";`)
-
-
-        if (idExist.length > 0) {
+        if (idExist) {
             res.status(400)
             throw new Error("O id informado já encontra-se em nossa base de dados.")
         }
 
-        if (emailExist.length > 0) {
+        if (emailExist) {
             res.status(400)
             throw new Error("O email informado já encontra-se em nossa base de dados.")
         }
 
-
-        db.raw(`INSERT INTO users (id, name, email, password) VALUES("${id}", "${name}", "${email}", "${password}");`).then(() => {
+        db('users').insert(newUser).then(() => {
             res.status(201).send(`${name}, seu cadastro foi realizado com sucesso!`)
         }).catch((err) => {
             res.status(500)
@@ -74,7 +70,7 @@ app.get('/users', async (req: Request, res: Response) => {
 
     try {
 
-        const result = await db.raw("SELECT * FROM users")
+        const result = await db('users')
 
         res.status(200).send(result)
 
@@ -107,28 +103,33 @@ app.post('/products', async (req: Request, res: Response) => {
         }
 
 
-        const idExist = await db.raw(`SELECT id FROM products WHERE id = "${id}";`)
-        const nameExist = await db.raw(`SELECT name FROM products WHERE name = "${name}";`)
+        const [idExist] = await db('products').where({ id })
+        const [nameExist] = await db('products').where({ name: name })
 
-        if (idExist.length > 0) {
+        if (idExist) {
             res.status(400)
             throw new Error(`O id '${id}', já consta em nossa base de dados.`)
         }
 
-        if (nameExist.length > 0) {
+        if (nameExist) {
             res.status(400)
             throw new Error(`Já existe um produto com o nome '${name}' em nossa base de dados.`)
         }
 
-
-        db.raw(`INSERT INTO products (id, name, price, description, image_url) VALUES ("${id}", "${name}", "${price}", "${description}", "${imageUrl}")`)
-            .then(() => {
-                res.status(201).send(`O produto ${name}, foi cadastrado com sucesso!`)
-            }).catch((err) => {
-                res.status(500)
-                throw new Error("Tivemos um problema para finalizar seu cadastro do seu produto, tente novamente. " + err)
-            })
-
+        db('products').insert(
+            {
+                id,
+                name,
+                price,
+                description,
+                image_url: imageUrl
+            }
+        ).then(() => {
+            res.status(201).send(`O produto ${name}, foi cadastrado com sucesso!`)
+        }).catch((err) => {
+            res.status(500)
+            throw new Error("Tivemos um problema para finalizar seu cadastro do seu produto, tente novamente. " + err)
+        })
 
     } catch (error: any) {
         res.send(error.message)
@@ -148,11 +149,11 @@ app.get('/products/:id', async (req: Request, res: Response) => {
             throw new Error(`Informe o id do produto a ser localizado`)
         }
 
-        const result = await db.raw(`SELECT * FROM products WHERE id = "${id}"`)
+        const [result] = await db('products').where({ id })
 
-        if (result.length < 1) {
+        if (!result) {
             res.status(404)
-            throw new Error(`O id informado não consta em nossa base de dados.`)
+            throw new Error(`O id '${id}' não consta em nossa base de dados.`)
         }
 
         res.status(200).send(result)
@@ -169,14 +170,10 @@ app.get('/products', async (req: Request, res: Response) => {
     try {
         const name = req.query.name
 
+
         if (typeof (name) !== "undefined") {
 
-            if (name.length === 0) {
-                res.status(422)
-                throw new Error("Informe o valor válido para o nome.")
-            }
-
-            const result = await db.raw(`SELECT * FROM products WHERE LOWER(name) LIKE LOWER('%${name}%');`)
+            const result = await db('products').where('name', 'LIKE', `%${name}%`)
 
             res.status(200).send(result)
 
@@ -192,6 +189,100 @@ app.get('/products', async (req: Request, res: Response) => {
     }
 
 
+})
+
+// Deletar um produto
+app.delete('/products/:id', async (req: Request, res: Response) => {
+
+    try {
+        const id = req.params.id
+        const regexId = /^prod\d{3}$/
+
+        if (!regexId.test(id)) {
+            res.status(400)
+            throw new Error(`Era esperado uma string seguindo o seguinte padrão: 'prod001', 'prod003', 'prod023', 'prod674'.`)
+        }
+
+
+        const [idExist] = await db('products').where({ id })
+
+        if (idExist) {
+            await db('products').del().where({ id }).then(() => {
+                res.status(300).send(`O produto "${idExist.name}" foi excluido com sucesso!`)
+            }).catch((err) => {
+                res.status(500)
+                throw new Error('Tivemos um problema para excluir o produto, tente novamente.' + err)
+            })
+        } else {
+            res.status(400)
+            throw new Error(`O id informado não encontra-se em nosso banco de dados.`)
+        }
+
+    } catch (error: any) {
+        res.send(error.message)
+    }
+
+})
+
+// Editar um produto
+app.put('/products/:id', async (req: Request, res: Response) => {
+
+    try {
+        const id = req.params.id
+        const { name, price, description, imageUrl } = req.body
+
+
+        const regexId = /^prod\d{3}$/
+
+        if (!regexId.test(id)) {
+            res.status(400)
+            throw new Error(`Era esperado uma string seguindo o seguinte padrão: 'prod001', 'prod003', 'prod023', 'prod674'.`)
+        }
+
+
+        Object.entries({ name, description, imageUrl }).map((item) => {
+            const [key, value] = item
+
+            if (value && typeof (value) !== "string") {
+                res.status(400)
+                throw new Error(`O parametro "${key}" espera receber uma "string" e foi enviado um valor do tipo "${typeof value}".`)
+            } else if (typeof (value) === "string" && value.length === 0) {
+                res.status(400)
+                throw new Error(`O parametro "${key}" não pode ser vazio".`)
+            }
+        })
+
+        if (price && isNaN(Number(price))) {
+            res.status(400)
+            throw new Error(`O parametro "price" espera receber um "number" e foi enviado um valor do tipo "${typeof price}".`)
+        }
+
+        const [productDb] = await db('products').where({ id })
+
+        if (productDb) {
+
+            const productEdited = {
+                name: name || productDb.name,
+                price: price || productDb.price,
+                description: description || productDb.description,
+                image_url: imageUrl || productDb.image_url
+            }
+
+            db('products').update(productEdited).where({ id }).then(() => {
+                res.status(201).send(`O Produto '${productEdited.name}' foi modificado com sucesso!`)
+            }).catch((err) => {
+                res.status(500)
+                throw new Error(`Tivemos um problema para editar o produto. ` + err)
+            })
+
+        } else {
+            res.status(400)
+            throw new Error(`O id '${id}' não consta em nossa base de dados.`)
+        }
+
+    } catch (error: any) {
+        res.send(error.message)
+    }
 })
 
 // Criar uma nova compra 
@@ -214,22 +305,26 @@ app.post('/purchases', async (req: Request, res: Response) => {
             throw new Error(`A propriedade 'totalPrice' deve ser um valor numérico, porém o valor recebido foi '${typeof (totalPrice)}'.`)
         }
 
-        const idExist = await db.raw(`SELECT id FROM purchases WHERE id = "${id}"`)
+        const [idExist] = await db('purchases').where({ id })
 
-        if (idExist.length > 0) {
+        if (idExist) {
             res.status(400)
             throw new Error(`O id '${id}' já consta em nossa base de dados.`)
         }
 
-        db.raw(`INSERT INTO purchases (id, buyer, total_price) VALUES ("${id}", "${buyer}", ${totalPrice});`).then(() => {
-
+        db('purchases').insert(
+            {
+                id,
+                buyer,
+                total_price: totalPrice
+            }
+        ).then(() => {
             res.status(201).send("Compra cadastrada com sucesso")
-
         }).catch((err) => {
-
             res.status(500)
             throw new Error("Tivemos um problema para finalizar sua compra, tente novamente. " + err)
         })
+
 
     } catch (error: any) {
         res.send(error.message)
@@ -243,9 +338,7 @@ app.get('/purchases/:id', async (req: Request, res: Response) => {
 
         const id = req.params.id
 
-        const [result] = await db.raw(
-            `SELECT * FROM purchases WHERE id = "${id}";`
-        )
+        const [result] = await db('purchases').where({ id })
 
         if (result) {
             res.status(200).send(result)
@@ -261,8 +354,7 @@ app.get('/purchases/:id', async (req: Request, res: Response) => {
 })
 
 // Deletar um usuario
-app.delete('/users/:id', (req: Request, res: Response) => {
-
+app.delete('/users/:id', async (req: Request, res: Response) => {
 
     try {
         const id = req.params.id
@@ -273,14 +365,16 @@ app.delete('/users/:id', (req: Request, res: Response) => {
             throw new Error(`Era esperado uma string seguindo o seguinte padrão: 'u001', 'u003', 'u023', 'u674'.`)
         }
 
-        const indexUser = users.findIndex((user) => {
-            return user.id === id
-        })
+        const [idExist] = await db('users').where({ id })
 
-        if (!indexUser) {
-            users.splice(indexUser, 1)
-            res.send("Usuário excluido com sucesso!")
+        if (idExist) {
+
+            db('users').del().where({ id }).then(() => {
+                res.status(300).send("Usuário excluido com sucesso!")
+            })
+
         } else {
+            res.status(400)
             throw new Error(`O id informado não encontra-se em nossa base de dados!`)
         }
 
@@ -292,92 +386,6 @@ app.delete('/users/:id', (req: Request, res: Response) => {
 
 })
 
-// Deletar um produto
-
-app.delete('/products/:id', (req: Request, res: Response) => {
-
-    try {
-        const id = req.params.id
-        const regexId = /^prod\d{3}$/
-
-        if (!regexId.test(id)) {
-            res.status(400)
-            throw new Error(`Era esperado uma string seguindo o seguinte padrão: 'prod001', 'prod003', 'prod023', 'prod674'.`)
-        }
-
-        const indexProduct = products.findIndex((product) => {
-            return product.id === id
-        })
-
-        if (!indexProduct) {
-            res.status(400)
-            throw new Error(`O id informado não encontra-se em nosso banco de dados.`)
-        }
-        const name = products[indexProduct].name
-        products.splice(indexProduct, 1)
-
-        res.status(200).send(`O produto "${name}" foi excluido com sucesso!`)
-
-    } catch (error: any) {
-        res.send(error.message)
-    }
-
-})
-
-// Editar um produto
-app.put('/products/:id', (req: Request, res: Response) => {
-
-    try {
-        const id = req.params.id
-        const { name, price, description, imageUrl } = req.body
-
-
-        const regexId = /^prod\d{3}$/
-
-        if (!regexId.test(id)) {
-            res.status(400)
-            throw new Error(`Era esperado uma string seguindo o seguinte padrão: 'prod001', 'prod003', 'prod023', 'prod674'.`)
-        }
-
-        const indexProduct = products.findIndex((product) => {
-            return product.id === id
-        })
-
-        if (indexProduct < 0) {
-            res.status(400)
-            throw new Error(`O id informado não encontra-se em nosso banco de dados.`)
-        }
-
-        const currentItem = products[indexProduct]
-
-        Object.entries({ name, description, imageUrl }).map((item) => {
-            const [key, value] = item
-
-            if (value && typeof (value) !== "string") {
-                res.status(400)
-                throw new Error(`O parametro "${key}" espera receber uma "string" e foi enviado um valor do tipo "${typeof value}".`)
-            } else if (typeof (value) === "string" && value.length === 0) {
-                res.status(400)
-                throw new Error(`O parametro "${key}" não pode ser vazio".`)
-            }
-        })
-
-        if (price && isNaN(Number(price))) {
-            res.status(400)
-            throw new Error(`O parametro "price" espera receber um "number" e foi enviado um valor do tipo "${typeof price}".`)
-        }
-
-        currentItem.name = name || currentItem.name
-        currentItem.price = price || currentItem.price
-        currentItem.description = description || currentItem.description
-        currentItem.imageUrl = imageUrl || currentItem.imageUrl
-
-        res.status(201).send(`Produto modificado com sucesso!`)
-
-    } catch (error: any) {
-        res.send(error.message)
-    }
-})
 
 app.listen(3003, () => {
     console.log("Servidor rodando na porta 3003")
